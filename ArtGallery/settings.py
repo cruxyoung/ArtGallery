85a818217ff30ccb696ede5ce5eee071369bdba5
Zixin Cheng <czxbnb@vip.qq.com>
2017-09-07 12:40:03 +1000

    Modified model
    Signed-off-by: Zixin Cheng <czxbnb@vip.qq.com>

--- ArtGallery/settings.py
+++ ArtGallery/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.auth',
-    'gelleryWeb',
 ]
 
 MIDDLEWARE = [
--- ArtGallery/urls.py
+++ ArtGallery/urls.py
@@ -17,7 +17,6 @@ from django.conf import settings
 from django.conf.urls import url, include
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
 
 
 from . import view
@@ -25,7 +24,6 @@ from . import view
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', view.hello),
-    url(r'^gelleryWeb/', include('gelleryWeb.urls', namespace='gelleryWeb')),
     url('^accounts/', include('django.contrib.auth.urls')),
 
 ]
