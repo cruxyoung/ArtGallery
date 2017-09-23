3d1ffd3bed7732b7becbd9bee6334f637c907956
max <zche4910@uni.sydney.edu.au>
2017-09-23 21:56:45 +1000

    left setting AJAX

--- ArtGallery/controllers/personal_controller.py
+++ ArtGallery/controllers/personal_controller.py
@@ -16,7 +16,7 @@ def personal_favorite_default(request):
     # get sort info
     sort = request.GET.get('sort', "")
     if sort:
-        if sort == "like_time":
+        if sort == "time":
             favs = favs.order_by('fav_time')
 
     # Paginator for favorite
@@ -27,6 +27,7 @@ def personal_favorite_default(request):
 
     p = Paginator(favs, 6, request=request)
     favs_pag = p.page(page)
+
     return render(request,
                   'personal_center/personal_center_favorites.html',
                   {'customer': request.user,
@@ -46,9 +47,8 @@ def personal_auctions_default(request):
     if sort:
         if sort == "time":
             auctions = auctions.order_by('-ah_aucTime')
-        # sort == 'price'
-        else:
-            auctions = auctions.order_by('ah_amount')
+        if sort == 'price':
+            auctions = auctions.order_by('-ah_amount')
 
     # Paginator for auctions
     try:
@@ -58,6 +58,7 @@ def personal_auctions_default(request):
 
     p = Paginator(auctions, 4, request=request)
     auctions_pag = p.page(page)
+
     return render(request,
                   'personal_center/personal_center_auctions.html',
                   {'customer': request.user,
@@ -68,34 +69,93 @@ def personal_auctions_default(request):
 
 
 def personal_rewards_default(request):
+    # get rewards history of current user
     rewards = models.Reward.objects.filter(customer_id=request.user.id)
+    rewards_nums = rewards.count()
+
+    # get sort info
+    sort = request.GET.get('sort', "")
+    if sort:
+        if sort == 'time':
+            rewards = rewards.order_by('-reward_time')
+        if sort == 'price':
+            rewards = rewards.order_by('-reward_amount')
+
+    # Paginator for rewards
+    try:
+        page = request.GET.get('page', 1)
+    except PageNotAnInteger:
+        page = 1
+
+    p = Paginator(rewards, 6, request=request)
+    rewards_pag = p.page(page)
+
     return render(request,
                   'personal_center/personal_center_rewards.html',
-                  {'rewards': rewards,
-                   'customer': request.user},
-                  )
+                  {'customer': request.user,
+                   'rewards': rewards_pag,
+                   'rewards_nums': rewards_nums,
+                   'sort': sort,
+                   })
 
 
 def personal_comments_default(request):
+    # get comments history of current user
     comments = models.Comment.objects.filter(commenter_id=request.user.id)
+    comments_nums = comments.count()
+
+    # get sort info
+    sort = request.GET.get('sort', "")
+    if sort:
+        if sort == 'time':
+            comments = comments.order_by('-comment_time')
+        if sort == 'comment':
+            comments = comments.filter(replay_commentId=0)
+        if sort == 'reply':
+            comments = comments.filter(rating=-1)
+
+    # Paginator for rewards
+    try:
+        page = request.GET.get('page', 1)
+    except PageNotAnInteger:
+        page = 1
+
+    p = Paginator(comments, 3, request=request)
+    comments_pag = p.page(page)
+
     return render(request,
                   'personal_center/personal_center_comments.html',
-                  {'comments': comments,
-                   'customer': request.user},
-                  )
+                  {'customer': request.user,
+                   'comments': comments_pag,
+                   'comments_nums': comments_nums,
+                   'sort': sort,
+                   })
 
 
 def personal_settings_default(request):
+    # get personal detail of current user
     return render(request,
                   'personal_center/personal_center_settings.html',
-                  {'customer': request.user},
-                  )
+                  {'customer': request.user})
 
 
 def personal_complaints_default(request):
+    # get complaints history of current user
     complaints = models.Complaint.objects.filter(customer_id=request.user.id)
+    complaints_nums = complaints.count()
+
+    # Paginator for rewards
+    try:
+        page = request.GET.get('page', 1)
+    except PageNotAnInteger:
+        page = 1
+
+    p = Paginator(complaints, 4, request=request)
+    comments_pag = p.page(page)
+
     return render(request,
                   'personal_center/personal_center_complaints.html',
-                  {'complaints': complaints,
-                   'customer': request.user},
-                  )
+                  {'customer': request.user,
+                   'complaints': comments_pag,
+                   'complaints_nums': complaints_nums,
+                   })
