2a56d8dfce5478d98e92c97dc3ba5993635d038f
cruxyoung <cruxyoung@hotmail.com>
2017-09-27 20:46:49 +1000

    jumping between home and art_list enabled

--- ArtGallery/views/home_page.py
+++ ArtGallery/views/home_page.py
@@ -4,20 +4,31 @@ from ArtGallery.forms import UserCreateForm
 from django.shortcuts import render, redirect
 from ..models import ArtWork
 from django import forms
+
+
 def home_page(request):
-
-
     artworks = ArtWork.objects.all()
-    return render(request, 'home_page/home_page.html', {'artworks':artworks})
+    artworks = artworks
+    print()
+    return render(request,
+                  'home_page/home_page.html',
+                  {'artworks': artworks,
+                   'example0':artworks[0],
+                   'example1': artworks[1],
+                   'example2': artworks[2],
+                   })
+
 
 def detail(request, art_id):
     return HttpResponse("You're looking at question %s." % art_id)
 
+
 # art_list page
 def art_list(request):
-    if request.method == "GET":
-
-        keyword = request.GET['keyword']
+    if request.method == "POST":
+        keyword = request.POST['keyword']
         print(keyword)
-        return HttpResponse('post')
-    return HttpResponse("hello,this is the list")
+
+
+
+    return render(request, 'home_page/art_list.html')
